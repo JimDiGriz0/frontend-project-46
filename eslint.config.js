bb37e1ce8731a22104f7697d31fd541cd02d9f2b
digriz <vakhitov.ms28@gmail.com>
2026-06-14 20:14:51 +0300

    complete step 6

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,16 @@ import js from '@eslint/js'
 import globals from 'globals'
 import { defineConfig } from 'eslint/config'
 import stylistic from '@stylistic/eslint-plugin'
+import configPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
+  {
+    ignores: ['coverage/**'],
+  },
   stylistic.configs.recommended,
   {
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
-    extends: ['js/recommended'],
     languageOptions: {
       globals: {
         ...globals.node,
@@ -16,4 +19,5 @@ export default defineConfig([
       },
     },
   },
+  configPrettier,
 ])
